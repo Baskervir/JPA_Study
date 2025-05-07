422a64e045339652ce2b6c5706a053fe189348ce
Baskervir <persona903@gmail.com>
2025-05-07 14:46:56 +0900

    TableGenerator strategy

--- src/main/java/hellojpa/Member/Member.java
+++ src/main/java/hellojpa/Member/Member.java
@@ -1,14 +1,21 @@
 package hellojpa.Member;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
     public Member() {
